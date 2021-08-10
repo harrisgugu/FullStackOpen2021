21df333f074a4a519d90e5a867b23bd74532c726
Tianlang Gu <78072288+harrisgugu@users.noreply.github.com>
2021-08-10 15:52:39 -0700

    Update App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,34 +26,33 @@ const Content = (props)=>{
 }
 const Total = (props)=>{
   return(
-    <p>Number of exercises : {props.total}</p>
+    <p>Number of exercises : {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
   )
 }
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
-    parts:[
+    name:'Half Stack application development',
+    parts : [
       {
-        name:'Fundamentals of React',
-        exercises: 10,
-      },{
-        name:'Using props to pass data',
-        exercises:7,
-
-      },{
-        name:'State of a component',
-        exercises:14,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
       }
-      
     ]
-   
   }
- 
+
   return (
     <div>
-      <Header name = {course.name}/>
-      <Content parts = {course.parts}/>
-      <Total total = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+      <Header name = {course.name} />
+      <Content parts=  {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
   )
 }
